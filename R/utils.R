afb5f253dd46561796f4cdb1d9155e971483e2a5
Luke W. Johnston <lwjohnst@gmail.com>
2022-12-11 17:49:46 +0100

    save to csv and assigning course version util functions

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,3 @@
-targets_output_switch <- function(object, path) {
-
-}
 #' Within date range, from lubridate
 #'
 #' @name %within%
@@ -12,3 +9,42 @@ targets_output_switch <- function(object, path) {
 #' @param rhs An interval object.
 #' @return A logical vector.
 NULL
+
+#' Save data to CSV.
+#'
+#' @param data Survey data.
+#' @param path Path where data should be saved to.
+#'
+#' @return Save a CSV file, returns tibble invisibly.
+#' @export
+#'
+save_as_csv <- function(data, path) {
+  fs::dir_create(fs::path_dir(path))
+  data |>
+    readr::write_csv(here::here(path))
+}
+
+#' Assign the version of the course that the survey response comes from.
+#'
+#' @param date The date of the survey response.
+#'
+#' @return A numeric value.
+#' @keywords internal
+#'
+#' @examples
+#' assign_course_version_by_date(c("2020-06-20", "2019-06-10"), metadata$dates$introduction)
+assign_course_version_by_date <- function(date, metadata_dates) {
+  metadata_dates <- as.character(metadata_dates)
+  dates_between_courses <- lubridate::interval(
+    c("2018-01-01", metadata_dates),
+    c(metadata_dates, as.character(lubridate::today()))
+  )
+  course_version <- date %>%
+    lubridate::as_date() %>%
+    lubridate::ymd() %>%
+    purrr::map_int(~ which(.x %within% dates_between_courses))
+  if (length(course_version) == 0) {
+    course_version <- NA_integer_
+  }
+  course_version
+}
