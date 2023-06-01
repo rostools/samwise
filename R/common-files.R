871a067b38b3c57f5b86f80fe2e5c0c5492b1429
Luke W. Johnston <lwjohnst@gmail.com>
2023-06-01 20:43:51 +0200

    use today() instead of now()

--- R/common-files.R
+++ R/common-files.R
@@ -7,7 +7,7 @@
 #'
 copy_common_file <- function(file = c("_variables.yml")) {
   template_path <- fs::path_package("r3admin", "common", file)
-  header_text <- glue::glue("# Automatically created by `r3admin::copy_common_file('{file}')` on {lubridate::now()}.")
+  header_text <- glue::glue("# Automatically created by `r3admin::copy_common_file('{file}')` on {lubridate::today()}.")
   if (rprojroot::is_rstudio_project$testfun[[1]](".")) {
     readr::write_lines(
       x = c(header_text, readr::read_lines(template_path)),
