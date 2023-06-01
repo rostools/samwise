c9cba35de8637ab543bc1d387226f1abf92d549d
Luke W. Johnston <lwjohnst@gmail.com>
2023-06-01 20:22:55 +0200

    add feedback extraction for intermediate course

--- data-raw/feedback-survey.R
+++ data-raw/feedback-survey.R
@@ -20,7 +20,23 @@ intro_feedback %>%
   extract_feedback_overall() %>%
   save_as_csv("data/intro/feedback-overall.csv")
 
-# Advanced feedback -------------------------------------------------------
+# Intermediate feedback ------------------------------------------------
+
+inter_feedback <- fetch_feedback_inter()
+
+inter_feedback %>%
+  extract_feedback_quantitative() %>%
+  save_as_csv("data/inter/feedback-quantitative.csv")
+
+inter_feedback %>%
+  extract_feedback_sessions() %>%
+  save_as_csv("data/inter/feedback-sessions.csv")
+
+inter_feedback %>%
+  extract_feedback_overall() %>%
+  save_as_csv("data/inter/feedback-overall.csv")
+
+# Advanced feedback ---------------------------------------------------
 
 advanced_feedback <- fetch_feedback_advanced()
 
