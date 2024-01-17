1d2219da23d51b36d7818fd1d9a70f8be88f2f6f
Luke W. Johnston <lwjohnst@gmail.com>
2024-01-17 16:49:04 +0100

    feat: Add functionality to extract fields from course metadata

--- R/course-metadata.R
+++ R/course-metadata.R
@@ -1,6 +1,6 @@
 
-list_courses <- function() {
+list_course_metadata <- function(field = c("id", "url", "github-repo", "name")) {
   courses_path <- fs::path_package(package = "r3admin", "data", "courses.yaml")
   yaml::read_yaml(courses_path)$course |>
-    purrr::pluck("")
+    purrr::map_chr(field)
 }
