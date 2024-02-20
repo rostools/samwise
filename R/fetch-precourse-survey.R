0a9b800fd1bb81bf801fd617699e1d6e46e7321a
Luke W. Johnston <lwjohnst@gmail.com>
2024-02-20 14:34:18 -0500

    chore: remove left over code

--- R/fetch-precourse-survey.R
+++ R/fetch-precourse-survey.R
@@ -1,22 +1,6 @@
 
 # Import pre-survey data --------------------------------------------------
 
-#' Fetch the pre-course survey responses.
-#'
-#' @name fetch_precourse
-#' @rdname fetch_precourse
-#' @param survey_id Google Forms ID for the survey.
-#'
-#' @return Data from survey, slightly tidied up.
-#'
-#' @examples
-#' \dontrun{
-#' fetch_precourse_intro()
-#' fetch_precourse_advanced()
-#' }
-#'
-NULL
-
 #' Get the (slightly cleaned) pre-course survey from Google Sheets.
 #'
 #' @param id The ID of the course, found by running [list_course_ids()].
@@ -52,38 +36,11 @@ get_precourse_survey <- function(id) {
   survey_id |>
     get_precourse_survey_google_sheet() |>
     rename_columns_sentence_to_snakecase(column_renaming) |>
-    tidy_precourse(get_course_dates(id))
+    tidy_precourse(get_course_dates(id)) |>
+    dplyr::mutate(course_id = id, .before = tidyselect::everything())
 }
 
-#' @describeIn fetch_precourse Fetch the pre-course survey data for the **introduction** course.
-#' @export
-fetch_precourse_intro <- function(survey_id = Sys.getenv("INTRO_PRE_SURVEY_ID")) {
-  survey_id |>
-    get_precourse_survey_google_sheet() |>
-    rename_columns_sentence_to_snakecase(intro_survey_column_renaming) |>
-    # check_duplicate_timestamps() %>%
-    tidy_precourse(get_course_dates("intro"))
-}
-
-#' @describeIn fetch_precourse Fetch the pre-course survey data for the **intermediate** course.
-#' @export
-fetch_precourse_inter <- function(survey_id = Sys.getenv("INTERMEDIATE_PRE_SURVEY_ID")) {
-  survey_id |>
-    get_precourse_survey_google_sheet() |>
-    rename_columns_sentence_to_snakecase(intermediate_survey_column_renaming) |>
-    # check_duplicate_timestamps() %>%
-    tidy_precourse(get_course_dates("inter"))
-}
-
-#' @describeIn fetch_precourse Fetch the pre-course survey data for the **advanced** course.
-#' @export
-fetch_precourse_advanced <- function() {
-  survey_id |>
-    get_precourse_survey_google_sheet() |>
-    rename_columns_sentence_to_snakecase(advanced_survey_column_renaming) |>
-    # check_duplicate_timestamps() %>%
-    tidy_precourse(get_course_dates("adv"))
-}
+# Get survey from Google --------------------------------------------------
 
 get_precourse_survey_google_sheet <- function(survey_id) {
   googledrive::drive_get(id = survey_id) |>
@@ -261,8 +218,6 @@ join_original_column_names <- function(data, column_renaming_df) {
     dplyr::select(-questions, questions = original_column_names) |>
     dplyr::relocate(questions)
 }
-
-# Checks ------------------------------------------------------------------
 
 # Renaming pre-course columns ---------------------------------------------
 
