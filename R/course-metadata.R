7ec7ae9e143f31e972c01696758df6c1cd3ca183
Luke W. Johnston <lwjohnst@gmail.com>
2024-03-07 22:34:49 +0100

    docs: and also some refactoring, of metadata get functions

--- R/course-metadata.R
+++ R/course-metadata.R
@@ -1,27 +1,47 @@
-#' Title
+#' Read in the course's metadata (as a YAML) saved in the package's `data/`
+#' folder.
 #'
 #' @return A (heavily nested) list.
 #' @export
 #'
 #' @examples
 #' library(purrr)
-#' read_course_metadata() |>
-#'   purrr::map(names) |>
-#'   unique()
+#' read_course_metadata()
 read_course_metadata <- function() {
   courses_path <- fs::path_package(package = "r3admin", "data", "courses.yaml")
   yaml::read_yaml(courses_path)$course
 }
 
+#' List all the IDs for the courses.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' list_course_ids()
 list_course_ids <- function() {
   read_course_metadata() |>
     purrr::map_chr("id") |>
     sort()
 }
 
+#' List all the metadata fields available in the first-level for each course.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' list_course_metadata_fields()
+list_course_metadata_fields <- function() {
+  read_course_metadata() |>
+    purrr::pluck(1) |>
+    names() |>
+    stringr::str_subset("id", negate = TRUE)
+}
+
 #' Metadata on the dates the courses have started on.
 #'
-#' @param course_id ID for the course (found in the `courses.yaml` file).
+#' @inheritParams get_course_metadata_field
 #'
 #' @return Character vector of dates.
 #' @export
@@ -30,24 +50,37 @@ list_course_ids <- function() {
 #' get_course_dates("inter")
 #' get_course_dates("intro")
 #' get_course_dates("adv")
-get_course_dates <- function(id = list_course_ids()) {
+get_course_dates <- function(id) {
   get_course_metadata_field(id, "events") |>
     purrr::map_chr(~ purrr::pluck(.x, "date")) |>
     sort()
 }
 
+#' General purpose function
+#'
+#' @param id The ID of the course, found by running [list_course_ids()].
+#' @param field The "key" value (field) for the ID of the course, found by running [list_course_metadata_fields()].
+#'
+#' @return A (nested) list.
+#' @export
+#'
+#' @examples
+#'
+#' get_course_metadata_field("intro", "events")
+#' get_course_metadata_field("intro", "date")
 get_course_metadata_field <- function(id, field) {
   id <- rlang::arg_match(id, list_course_ids())
+  field <- rlang::arg_match(field, list_course_metadata_fields())
   read_course_metadata() |>
     purrr::keep(~ .x$id == id) |>
     purrr::pluck(1, field)
 }
 
-#' Title
+#' Get the dates for the next course.
 #'
-#' @param id
+#' @inheritParams get_course_metadata_field
 #'
-#' @return
+#' @return A character vector.
 #' @export
 #'
 #' @examples
@@ -55,13 +88,13 @@ get_course_metadata_field <- function(id, field) {
 #' get_upcoming_course_dates("intro")
 #' get_upcoming_course_dates("inter")
 #' get_upcoming_course_dates("adv")
-get_upcoming_course_dates <- function(id = list_course_ids()) {
+get_upcoming_course_dates <- function(id) {
   get_course_dates(id = id) |>
     purrr::keep(~ .x >= lubridate::today())
 }
 
 get_upcoming_course <- function() {
-  course_metadata <- read_course_metadata()
+  # course_metadata <- read_course_metadata()
 
   # upcoming <-
     # course_metadata |>
