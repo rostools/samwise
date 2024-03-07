e3ef866ce48a885d43325e42bd19f3c943db4984
Luke W. Johnston <lwjohnst@gmail.com>
2024-03-07 22:58:05 +0100

    docs: add roxygen docs for the function to get schedule

--- R/list-sessions.R
+++ R/list-sessions.R
@@ -1,5 +1,14 @@
 
-get_schedule_sessions <- function(id = list_course_ids()) {
+#' Get the session schedule for the course.
+#'
+#' @inheritParams get_course_metadata_field
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' get_schedule_sessions("intro")
+get_schedule_sessions <- function(id) {
   id <- rlang::arg_match(id, list_course_ids())
   course_repo <- get_course_repo(id = id)
   base_url <- "https://raw.githubusercontent.com/rostools"
@@ -7,6 +16,7 @@ get_schedule_sessions <- function(id = list_course_ids()) {
   full_url <- glue::glue("{base_url}/{course_repo}/main/{schedule_path}")
   readr::read_csv(full_url, show_col_types = FALSE) |>
     dplyr::filter(icon %in% c("laptop-code", "person-chalkboard")) |>
+    dplyr::arrange(Day, Time) |>
     dplyr::select(day = Day, topic = `Session topic`) |>
     dplyr::mutate(topic = stringr::str_remove(topic, "\\(with short break\\)") |>
                     stringr::str_trim()) |>
