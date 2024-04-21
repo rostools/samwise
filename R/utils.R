110f6f98f02a32ca9e45c3f5be2c27f0cc62dab6
Luke W. Johnston <lwjohnst@gmail.com>
2024-04-22 00:00:57 +0200

    fix: properly assign a course for those who finished the precourse tasks on the first day

--- R/utils.R
+++ R/utils.R
@@ -18,18 +18,27 @@ NULL
 #' @keywords internal
 #'
 #' @examples
-#' assign_course_date_by_date(c("2020-06-20", "2019-06-10", "2021-06-10"),
-#'   get_course_dates("intro"))
+#' assign_course_date_by_date(
+#'   c("2020-06-20", "2019-06-10", "2021-06-10"),
+#'   get_course_dates("intro")
+#' )
 assign_course_date_by_date <- function(date, metadata_dates) {
   metadata_dates <- as.character(metadata_dates)
   dates_between_courses <- lubridate::interval(
     c("2018-01-01", metadata_dates),
     c(metadata_dates, as.character(lubridate::today() + lubridate::weeks(4)))
   )
-  course_date <- date %>%
+  date <- date %>%
     lubridate::as_date() %>%
-    lubridate::ymd() %>%
-    purrr::map_chr(~ metadata_dates[which(.x %within% dates_between_courses)])
+    lubridate::ymd()
+
+  course_date <- date |>
+    purrr::map_chr(\(date)
+    if (date %in% metadata_dates) {
+      stringr::str_subset(metadata_dates, as.character(date))
+    } else {
+      metadata_dates[which(date %within% dates_between_courses)]
+    })
   if (length(course_date) == 0) {
     course_date <- NA_integer_
   }
