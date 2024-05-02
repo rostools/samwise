cfa45b84b05cddc49257f5109365c68cfdb46693
Luke W. Johnston <lwjohnst@gmail.com>
2024-05-02 09:02:51 +0200

    fix: changed variable names after update to new processing

--- R/checks.R
+++ R/checks.R
@@ -34,14 +34,16 @@ copy_reminder_email_text <- function() {
 
 check_precourse_problems <- function(data) {
   data %>%
-    dplyr::filter(encounter_problems == "Yes") %>%
-    dplyr::select(tidyselect::any_of(c(
+    dplyr::filter(
+      did_you_encounter_any_problems_during_the_pre_course_tasks == "Yes"
+    ) %>%
+    dplyr::select(tidyselect::contains(c(
       "full_name",
       "email",
-      "check_setup_output",
-      "check_project_setup_output",
-      "describe_problems",
-      "when_available_for_help"
+      "check_setup",
+      "check_project_setup",
+      "describe_the_problems",
+      "would_you_be_available"
     )))
 }
 
@@ -65,28 +67,44 @@ pretty_text_checks <- function(name, email, check) {
 
 check_setup <- function(data) {
   data %>%
+    dplyr::select(
+      full_name = what_is_your_full_name,
+      email = email_address,
+      check_setup = copy_and_paste_the_results_of_your_r_3_check_setup_into_the_text_box_below
+    ) |>
     dplyr::group_split(full_name) %>%
     purrr::map_chr(~ pretty_code_checks(
       .x$full_name,
       .x$email,
-      .x$check_setup_output
+      .x$check_setup
     )) %>%
     stringr::str_c(collapse = "\n\n")
 }
 
 check_project_setup <- function(data) {
   data %>%
+    dplyr::select(
+      full_name = what_is_your_full_name,
+      email = email_address,
+      check_project_setup = copy_and_paste_the_results_of_your_r_3_check_project_setup_into_the_text_box_below
+    ) |>
     dplyr::group_split(full_name) %>%
     purrr::map_chr(~ pretty_code_checks(
       .x$full_name,
       .x$email,
-      .x$check_project_setup_output
+      .x$check_project_setup
     )) %>%
     stringr::str_c(collapse = "\n\n\n")
 }
 
 check_problem_description <- function(data) {
   data %>%
+    dplyr::select(
+      full_name = what_is_your_full_name,
+      email = email_address,
+      describe_problems = please_describe_the_problems_you_ve_had,
+      when_available_for_help = which_dates_would_you_be_available_for_a_video_call_to_help_with_the_problems
+    ) |>
     dplyr::group_split(full_name) %>%
     purrr::map_chr(~ pretty_text_checks(
       .x$full_name,
