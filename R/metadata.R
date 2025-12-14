cd53ce9c00cb8f1cf53be0d322361505cc2c763c
Luke W. Johnston <lwjohnst@gmail.com>
2025-12-14 20:07:37 +0100

    chore: :truck: rename to simply "metadata"

--- R/metadata.R
+++ R/metadata.R
@@ -7,7 +7,11 @@
 #' @examples
 #' read_course_metadata()
 read_course_metadata <- function() {
-  courses_path <- fs::path_package(package = "samwise", "extdata", "courses.yaml")
+  courses_path <- fs::path_package(
+    package = "samwise",
+    "extdata",
+    "workshops.yaml"
+  )
   yaml::read_yaml(courses_path)$course
 }
 
