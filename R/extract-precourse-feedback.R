b6b3469333d00429fe2ca2b3e7b044dab0997f7a
Luke W. Johnston <lwjohnst@gmail.com>
2026-01-31 10:44:04 +0100

    refactor: :recycle: can use quotes when using dplyr verbs

--- R/extract-precourse-feedback.R
+++ R/extract-precourse-feedback.R
@@ -19,7 +19,7 @@ extract_preworkshop_feedback <- function(data) {
     anonymize_preworkshop() |>
     dplyr::select(
       "workshop_id",
-      .data$workshop_date,
+      "workshop_date",
       tidyselect::matches("worked.*well"),
       tidyselect::matches("could.*improved"),
       tidyselect::matches("describe.*problems"),
@@ -27,19 +27,19 @@ extract_preworkshop_feedback <- function(data) {
       tidyselect::matches("why.*attend")
     ) |>
     tidyr::pivot_longer(
-      -c(.data$workshop_id, .data$workshop_date),
+      -c(workshop_id, workshop_date),
       names_to = "question",
       values_to = "response"
     ) |>
     remove_newlines("response") |>
-    dplyr::arrange(.data$workshop_date, .data$question, .data$response) |>
+    dplyr::arrange("workshop_date", "question", "response") |>
     join_original_column_names() |>
     tidyr::drop_na() |>
     dplyr::relocate(
-      .data$workshop_id,
-      .data$workshop_date,
-      .data$question,
-      .data$response
+      "workshop_id",
+      "workshop_date",
+      "question",
+      "response"
     ) |>
     drop_missing_responses()
 }
